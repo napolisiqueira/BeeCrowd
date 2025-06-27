0a1f742ce6f0e2eaad6894a0c55c2d7e987f5853
napolisiqueira <imfelipenapoli@gmail.com>
2025-06-26 23:59:44 -0300

    1157 exercise of BeeCrowd

---
+++ BeecrowdPy/Iniciante/1157.py
@@ -0,0 +1,18 @@
+"""
+
+Ler um número inteiro N e calcular todos os seus divisores.
+
+Entrada
+O arquivo de entrada contém um valor inteiro.
+
+Saída
+Escreva todos os divisores positivos de N, um valor por linha.
+
+
+"""
+
+N = int(input())
+
+for i in range(1, N+1):
+    if N % i == 0:
+        print(i)
